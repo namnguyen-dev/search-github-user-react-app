114354e2856f1991f096d504ea01238412bc4358
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-04-06 19:24:38 +0700

    set up context api

--- src/context/context.js
+++ src/context/context.js
@@ -5,3 +5,11 @@ import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+const GithubContext = React.createContext()
+
+const GithubProvider = ({children}) => {
+    return <GithubContext.Provider value={'hello'}>{children}</GithubContext.Provider>
+}
+
+export {GithubContext,GithubProvider}
