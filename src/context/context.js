fd3bba4d6e661b54491a45938ee996705f39c176
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-04-06 20:31:12 +0700

    add mockdata to context api, set up Info conponent

--- src/context/context.js
+++ src/context/context.js
@@ -6,10 +6,23 @@ import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
 
-const GithubContext = React.createContext()
+const GithubContext = React.createContext();
 
-const GithubProvider = ({children}) => {
-    return <GithubContext.Provider value={'hello'}>{children}</GithubContext.Provider>
-}
+const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
+  return (
+    <GithubContext.Provider
+      value={{
+        githubUser,
+        repos,
+        followers,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
 
-export {GithubContext,GithubProvider}
+export { GithubContext, GithubProvider };
