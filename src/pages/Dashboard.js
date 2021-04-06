325eb42f1e6d63ac87a39dc90f50aa27adbcecee
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-04-06 20:58:26 +0700

    set up User component

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -6,11 +6,11 @@ const Dashboard = () => {
 
   return (
     <main>
-      <Navbar/>
-      <Search/>
+      {/* <Navbar/> */}
+      {/* <Search/> */}
       <Info/>
       <User/>
-      <Repos/>
+      {/* <Repos/> */}
     </main>
   );
 };
