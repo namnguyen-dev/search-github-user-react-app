6f265147145261c8a9935de92ebad82b5daada3c
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-04-08 20:13:23 +0700

    add AuthWrapper to App component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,13 +3,20 @@ import { useAuth0 } from '@auth0/auth0-react';
 import styled from 'styled-components';
 import loginImg from '../images/login-img.svg';
 const Login = () => {
-  return <Wrapper>
-    <div className="container">
-      <img src={loginImg} alt="github-user"/>
-      <h1>github user</h1>
-      <button className='btn'>login</button>
-    </div>
-  </Wrapper>
+  const { loginWithRedirect } = useAuth0();
+ 
+
+  return (
+    <Wrapper>
+      <div className="container">
+        <img src={loginImg} alt="github-user" />
+        <h1>github user</h1>
+        <button className="btn" onClick={loginWithRedirect}>
+          login / sign up
+        </button>
+      </div>
+    </Wrapper>
+  );
 };
 const Wrapper = styled.section`
   min-height: 100vh;
