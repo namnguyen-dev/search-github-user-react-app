252cbb56ce2b894a9a1623aa868d8da05973def1
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-04-08 16:54:15 +0700

    refactor axios request

--- src/context/context.js
+++ src/context/context.js
@@ -24,21 +24,26 @@ const GithubProvider = ({ children }) => {
     const response = await axios(`${rootUrl}/users/${user}`).catch(err => {
       console.log(err);
     });
-    console.log(response);
 
     if (response) {
       setGithubUser(response.data);
       const { login, followers_url } = response.data;
-      // repos
-      axios(`${rootUrl}/users/${login}/repos?per_page=100`).then(response =>
-        setRepos(response.data)
-      );
 
-      // followers
-      axios(`${followers_url}?per_page=100`).then(response =>
-        setFollowers(response.data)
-      );
-
+      await Promise.allSettled([
+        axios(`${rootUrl}/users/${login}/repos?per_page=100`),
+        axios(`${followers_url}?per_page=100`),
+      ])
+        .then(results => {
+          const [repos, followers] = results;
+          const status = 'fulfilled';
+          if (repos.status === status) {
+            setRepos(repos.value.data);
+          }
+          if (followers.status === status) {
+            setFollowers(followers.value.data);
+          }
+        })
+        .catch(err => console.log(err));
     } else {
       toggleError(true, 'there is no user with that username');
     }
